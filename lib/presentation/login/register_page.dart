02c9d913efda5c2f242b30c2ed5a9041b193c8e1
Frank Pomiano <frank.pomiano@unmsm.edu.pe>
2022-11-11 00:27:04 -0500

    Validacion de campos de registro de usuario 1

--- lib/presentation/login/register_page.dart
+++ lib/presentation/login/register_page.dart
@@ -6,18 +6,15 @@ class RegisterPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Color.fromRGBO(245, 245, 252, 1.0),
+        backgroundColor: const Color.fromRGBO(245, 245, 252, 1.0),
         body: SingleChildScrollView(
           child: Column(
             children: [
               SizedBox(height: 80),
-              Container(
-                //decoration: BoxDecoration(color: Colors.blue),
-                child: Center(
-                  child: Text(
-                    '¡Registrese!',
-                    style: TextStyle(fontFamily: 'Inder', fontSize: 30),
-                  ),
+              const Center(
+                child: Text(
+                  '¡Registrese!',
+                  style: TextStyle(fontFamily: 'Inder', fontSize: 30),
                 ),
               ),
               SizedBox(height: 80),
@@ -26,32 +23,76 @@ class RegisterPage extends StatelessWidget {
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 50, vertical: 0),
                     child: Form(
+                      autovalidateMode: AutovalidateMode.onUserInteraction,
                       child: Column(
                         children: [
                           Container(
-                            decoration: BoxDecoration(
-                                color: Colors.white,
-                                borderRadius: BorderRadius.circular(30)),
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: box_decoration(),
                             child: TextFormField(
-                              decoration: InputDecoration(
-                                  border: InputBorder.none,
-                                  hintText: 'Nombres y apellidos',
-                                  hintStyle: TextStyle(
-                                      fontFamily: 'Inder',
-                                      color: Color.fromRGBO(0, 118, 135, 0.3))),
+                              decoration: input_decoration('Nombres'),
+                              validator: name_validator,
                             ),
                           ),
-                          SizedBox(height: 20),
-                          TextFormField(),
-                          SizedBox(height: 20),
-                          TextFormField(),
-                          SizedBox(height: 20),
-                          TextFormField(),
-                          SizedBox(height: 20),
+                          SizedBox(height: 17),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: box_decoration(),
+                            child: TextFormField(
+                              decoration: input_decoration('Apellidos'),
+                              validator: name_validator,
+                            ),
+                          ),
+                          SizedBox(height: 17),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: box_decoration(),
+                            child: TextFormField(
+                              decoration:
+                                  input_decoration('Correo Electronico'),
+                              validator: (value) {
+                                String pattern =
+                                    r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                                RegExp regExp = new RegExp(pattern);
+                                return regExp.hasMatch(value ?? '')
+                                    ? null
+                                    : 'El valor ingresado no es un correo';
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 17),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: box_decoration(),
+                            child: TextFormField(
+                              decoration: input_decoration('Contraseña'),
+                            ),
+                          ),
+                          SizedBox(height: 17),
+                          Container(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            decoration: box_decoration(),
+                            child: TextFormField(
+                              decoration:
+                                  input_decoration('Confirmar contraseña'),
+                            ),
+                          ),
+                          SizedBox(height: 27),
                           MaterialButton(
                               shape: RoundedRectangleBorder(
-                                  borderRadius: BorderRadius.circular(15)),
+                                  borderRadius: BorderRadius.circular(23)),
                               color: Colors.green[900],
+                              child: Container(
+                                padding: EdgeInsets.symmetric(
+                                    horizontal: 65, vertical: 15),
+                                child: Text(
+                                  'Registrarme',
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontFamily: 'Inder',
+                                      fontSize: 16),
+                                ),
+                              ),
                               onPressed: () {})
                         ],
                       ),
@@ -59,16 +100,48 @@ class RegisterPage extends StatelessWidget {
                   ),
                   Container(
                     child: Center(
-                        child: Text('¿Ya tienes una cuenta?',
-                            style: TextStyle(
-                                fontFamily: 'Inder',
-                                fontSize: 14,
-                                color: Colors.blueAccent))),
-                  )
+                        child: Container(
+                      padding: EdgeInsets.symmetric(vertical: 25),
+                      child: Text('¿Ya tienes una cuenta?',
+                          style: TextStyle(
+                              fontFamily: 'Inder',
+                              fontSize: 14,
+                              color: Colors.blueAccent)),
+                    )),
+                  ),
+                  Image.asset("assets/images/Logo.png")
                 ],
               ),
             ],
           ),
         ));
+  }
+
+  String? name_validator(value) {
+    String patternletters = r'^[a-zA-Z]+$';
+    RegExp regExp = new RegExp(patternletters);
+    if (value != null && !regExp.hasMatch(value.trim())) {
+      return 'Debe contener solo letras';
+    }
+    if (value != null && value.trim().length < 5) {
+      return 'Debe ser mayor a 5 caracteres';
+    } else if (value != null && value.trim().length > 30) {
+      return 'Debe ser menor a 30 caracteres';
+    } else {
+      return null;
+    }
+  }
+
+  BoxDecoration box_decoration() {
+    return BoxDecoration(
+        color: Colors.white, borderRadius: BorderRadius.circular(10));
+  }
+
+  InputDecoration input_decoration(String hinttext) {
+    return InputDecoration(
+        border: InputBorder.none,
+        hintText: hinttext,
+        hintStyle: TextStyle(
+            fontFamily: 'Inder', color: Color.fromRGBO(0, 118, 135, 0.3)));
   }
 }
