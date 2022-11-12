65656680d7c9c22463655be2408a1747b9df7507
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-11-12 08:23:53 -0500

    To fix: Asset/LogoHeader.png

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,23 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:softales/main.dart';
 
 void main() {
-//   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
-//     // Build our app and trigger a frame.
-//     await tester.pumpWidget(const MyApp());
-
-//     // Verify that our counter starts at 0.
-//     expect(find.text('0'), findsOneWidget);
-//     expect(find.text('1'), findsNothing);
-
-//     // Tap the '+' icon and trigger a frame.
-//     await tester.tap(find.byIcon(Icons.add));
-//     await tester.pump();
-
-//     // Verify that our counter has incremented.
-//     expect(find.text('0'), findsNothing);
-//     expect(find.text('1'), findsOneWidget);
-//   });
+  testWidgets('Check rendering', (WidgetTester tester) async {
+    // Starting
+    await tester.pumpWidget(const MyApp());
+    expect(find.text('Vamos a empezar'), findsOneWidget);
+    expect(find.text('ACCEDER'), findsOneWidget);
+    await tester.tap(find.text('ACCEDER'));
+    await tester.pumpAndSettle();
+    // Home Page
+    expect(find.text('Crear Historia'), findsOneWidget);
+    expect(find.text('INICIO'), findsOneWidget);
+    expect(find.text('HISTORIAS'), findsOneWidget);
+    await tester.tap(find.text('HISTORIAS'));
+    await tester.pumpAndSettle();
+    // Tales page
+    expect(find.text('Search'), findsOneWidget);
+  });
 }
