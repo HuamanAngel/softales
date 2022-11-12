b457d9835e1da840451309dc4343dc0e3f5b338e
Alexis Marcos <renzo.marcos@unmsm.edu.pe>
2022-11-11 21:59:03 -0500

    Crear Historia

--- lib/presentation/home/collection/collection_page.dart
+++ lib/presentation/home/collection/collection_page.dart
@@ -10,6 +10,55 @@ class CollectionPage extends StatefulWidget {
 class _CollectionPageState extends State<CollectionPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(child: Text("Collection page"));
+    return SingleChildScrollView(
+        child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                style: ButtonStyle(
+                  backgroundColor:
+                      MaterialStateProperty.all<Color>(Colors.green.shade300),
+                  padding: MaterialStateProperty.all<EdgeInsetsGeometry>(
+                      const EdgeInsets.all(20)),
+                  textStyle: MaterialStateProperty.all<TextStyle>(
+                      const TextStyle(fontSize: 20)),
+                ),
+                child: const Text("Crear Historia"),
+              ),
+            ],
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+          Image(
+            image: const AssetImage("assets/images/Logo.png"),
+            width: MediaQuery.of(context).size.width * 0.1,
+          ),
+        ]));
   }
 }
