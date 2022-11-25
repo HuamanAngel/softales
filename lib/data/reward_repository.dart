0074997d9d97be7be8fbc185071fd2bc8f6b9d75
Renzo Ruiz <renzo.ruizcastro@hotmail.com>
2022-11-24 21:09:02 -0500

    fix: main appbar, home listview.builder

--- lib/data/reward_repository.dart
+++ lib/data/reward_repository.dart
@@ -1,23 +1,23 @@
-import 'package:softales/domain/entities/user.dart';
+// import 'package:softales/domain/entities/user.dart';
 
-abstract class UserRepository {
-  Future<User> authenticate({
-    required String username,
-    required String password,
-  });
-}
+// abstract class UserRepository {
+//   Future<User> authenticate({
+//     required String username,
+//     required String password,
+//   });
+// }
 
-class UserRepositoryImpl extends UserRepository {
-  @override
-  Future<User> authenticate({
-    required String username,
-    required String password,
-  }) async {
-    await Future.delayed(const Duration(seconds: 1));
-    return User(
-      email: "asddasdas",
-      name: 'John Doe',
-      level: 66,
-    );
-  }
-}
+// class UserRepositoryImpl extends UserRepository {
+//   @override
+//   Future<User> authenticate({
+//     required String username,
+//     required String password,
+//   }) async {
+//     await Future.delayed(const Duration(seconds: 1));
+//     return User(
+//       email: "asddasdas",
+//       name: 'John Doe',
+//       level: 66,
+//     );
+//   }
+// }
